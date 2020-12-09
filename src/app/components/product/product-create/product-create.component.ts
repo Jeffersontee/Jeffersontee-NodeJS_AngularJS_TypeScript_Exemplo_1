817b772262f4cb7ff7e6ea0aaee4dfe5c1e3d98f
Jefferson gomes <jefferson.silva@medral.com.br>
2020-12-08 23:36:16 -0300

    CrudAngular

---
+++ src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,45 @@
+import { Product } from './../product.model';
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  propLegal = "qualquer"
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(private productService: ProductService, 
+    private router: Router) { }
+
+  ngOnInit(): void {
+  
+  }
+
+  fazerAlgo(): void {
+    console.log('Fazendo algo!')
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+
+    
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
